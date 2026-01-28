7f2ffdd9aeac12125d9396b38e33bb05dcaf034a
qlghs8-lang <qlghs8@gmail.com>
2026-01-28 18:40:29 +0900

    Update JwtFilter.java

--- src/main/java/com/example/recommend/config/JwtFilter.java
+++ src/main/java/com/example/recommend/config/JwtFilter.java
@@ -69,8 +69,8 @@ public class JwtFilter extends OncePerRequestFilter {
             // 토큰 문제면 인증 제거 -> 결과적으로 401/403
             SecurityContextHolder.clearContext();
 
-            // ✅ 선택(권장): 개발 중 디버깅 편하게 명시적으로 401 반환
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
     }
 }
+
